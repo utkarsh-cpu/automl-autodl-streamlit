84c030eb92b003160f95b5962a0186efa0a8d8e4
Utkarsh Kaushik <54211424+utkarsh-cpu@users.noreply.github.com>
2021-12-26 19:02:36 +0530

    Done Model Making and committing on streamlit
    Model Making Done, Streamlit part to be finished

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+data_file=st.file_uploader("Choose A File")
+
+
+
---
+++ model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import sklearn.datasets as sk
+from tensorflow import keras
+
+
+def model_making(train_data,train_value,nooflayers,length,activation_function,c_r):
+    model=keras.Sequential()
+    model.add(tf.keras.Input(size=(len(train_data[0]),)))
+    for i in range(nooflayers):
+        model.add(tf.keras.Dense(length[i],activation=activation_function[i]))
+    if(c_r=='c'):
+        model.add(tf.keras.Dense(len(train_value.value_counts())))
+    elif(c_r=='r'):
+        model.add(tf.keras.Dense(len(train_value[0])))
+    
+    return model
+
