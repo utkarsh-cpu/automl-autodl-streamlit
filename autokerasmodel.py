6f601fed461d9caf1c5cc1eded34d873e3cde628
Utkarsh Kaushik <54211424+utkarsh-cpu@users.noreply.github.com>
2022-01-09 03:54:18 +0530

    Important changes for autokeras

--- autokerasmodel.py
+++ autokerasmodel.py
@@ -7,10 +7,10 @@ def encoded_data(data_values):
     return onehot_encoded
 
 def autokeras_classification(data,values,data_val,value_val,test_data,test_values,no_of_model_to_be_checked,epoch,loss_func,metrics):
-    model=ak.StructuredDataClassifier(max_trials=no_of_model_to_be_checked,loss=loss_func,metrics=metrics,objective='val_accuracy')
-    model.fit(data,encoded_data(values),validation_data=(data_val,value_val),epochs=epoch)
+    model=ak.StructuredDataClassifier(max_trials=no_of_model_to_be_checked,loss=loss_func,metrics=metrics)
+    model.fit(data,encoded_data(values),validation_data=(data_val,encoded_data(value_val)),epochs=epoch)
     predict_y=model.predict(test_data)
-    metrics_value=(model.evaluate(test_data,test_values))
+    metrics_value=(model.evaluate(test_data,encoded_data(test_values)))
     model_best = model.export_model()
     return predict_y,metrics_value,model_best
 
--- model.py
+++ model.py
@@ -12,14 +12,14 @@ def encoded_data(data_values):
 
 def model_making(train_data,train_value,nooflayers,length,activation_function,c_r,optimizers,loss_function,metrics,epoch):
     model=keras.Sequential()
-    model.add(tf.keras.Input(shape=(len(train_data[0]),)))
+    model.add(tf.keras.Input(shape=(len(train_data.columns),)))
     for i in range(nooflayers):
         model.add(tf.keras.layers.Dense(length[i],activation=activation_function[i]))
     if(c_r=='c'):
         model.add(tf.keras.layers.Dense(len(pd.DataFrame(train_value).value_counts()),activation=activation_function[-1]))
         model.compile(optimizer=optimizers,loss=loss_function,metrics=metrics)
     elif(c_r=='r'):
-        model.add(tf.keras.layers.Dense(len(train_value[0])))
+        model.add(tf.keras.layers.Dense(len(train_value.columns)))
         model.compile(optimizer=optimizers,loss=loss_function,metrics=metrics)
     return model
     
